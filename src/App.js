cc7c868360cde5bf99e678a3b01b14a088f62df2
Student <stu@ravtechj.local>
2018-11-01 19:19:54 +0200

    1/11/18 7:22

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 // import {button,Alert,Input,te}from 'reactstrap'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import Connection from './myconect/Connection'
-import axios from 'axios';
+import axios from "axios";
 
-import MySelect from './components/mySelect';
-
-
+import MySelect from "./components/mySelect";
 
 class App extends Component {
-state ={risk:{}}
+  state = { risks:[{riskName:"delay"},{riskName:"test"},{riskName:"budget"},{riskName:"customer"}]  };
 
-addRiskToDataBase= (e) =>{
-let name = e.target.elements.riskName.value;
-console.log(name);
+  addRiskToDataBase = e => {
+    let name = e.target.elements.riskName.value;
+    console.log(name);
+  };
 
+  componentDidMount() {
+    axios.get("/EndPage").then(response => {
+      this.setState({
+        obj: response.data.arrayResult
+      });
+      console.log(this.state.obj);
 
-}
+      // console.log((response.data));
+    });
+  }
 
-    componentDidMount() {
-
-      axios.get("/EndPage")
-        .then((response) => {
-  
-  
-          this.setState({
-            obj: response.data.arrayResult
-          })
-          console.log(this.state.obj)
-            
-          // console.log((response.data));
-        })
-    }
-  
   render() {
+
+    const mySelectsList = this.state.risks.map((item, index) =>
+      <MySelect riskName={item.riskName} myMethod ={this.addRiskToDataBase}   />
+    )
     return (
       <div className="App">
-       
-
-     <MySelect riskName = "delay"  myMethod = {this.addRiskToDataBase}/>
-     <MySelect riskName = "test" myMethod = {this.addRiskToDataBase}/>
-     <MySelect riskName = "budget" myMethod = {this.addRiskToDataBase}/>
-     <MySelect riskName = "customer"myMethod = {this.addRiskToDataBase} />
-       
+        
+        {mySelectsList}
 
 
-          <button className="save"  type="submit" onClick={this.postHandle}>save</button>
-
-       
-
-
-
+        <button className="save" type="submit" onClick={this.postHandle}>
+          save
+        </button>
       </div>
     );
   }
